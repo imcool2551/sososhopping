3b19067855dea4a8f6a772610741cf24cb51797d
SangWook <imcool2551@gmail.com>
2021-11-01 19:13:07 +0900

    template with sample image, logout call should be POST

--- src/main/java/com/sososhopping/server/controller/AdminController.java
+++ src/main/java/com/sososhopping/server/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.sososhopping.server.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
 @Controller
@@ -14,7 +15,32 @@ public class AdminController {
     }
 
     @GetMapping("/admin/login")
-    private String login() {
+    private String loginForm() {
         return "admin/login";
+    }
+
+    @PostMapping("/admin/login")
+    private String login() {
+        return "/admin/login";
+    }
+
+    @GetMapping("/admin/store-register")
+    private String storeRegister() {
+        return "/admin/store-register";
+    }
+
+    @GetMapping("/admin/user-report")
+    private String userReport() {
+        return "/admin/user-report";
+    }
+
+    @GetMapping("/admin/store-report")
+    private String storeReport() {
+        return "/admin/store-report";
+    }
+
+    @PostMapping("/admin/logout")
+    private String logout() {
+        return "/admin/login";
     }
 }
