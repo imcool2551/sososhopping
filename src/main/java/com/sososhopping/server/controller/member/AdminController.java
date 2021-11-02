1394db1c5aae2b64224c09aa6dfda554f883f266
SangWook <imcool2551@gmail.com>
2021-11-02 12:15:17 +0900

    moved AdminController into com.sososhopping.server.controller.member package

--- src/main/java/com/sososhopping/server/controller/member/AdminController.java
+++ src/main/java/com/sososhopping/server/controller/member/AdminController.java
@@ -1,4 +1,4 @@
-package com.sososhopping.server.controller;
+package com.sososhopping.server.controller.member;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
