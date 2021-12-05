cabc2835506ad8a09e7765c1bdbd71767cdd1aac
SangWook <imcool2551@gmail.com>
2021-12-06 03:30:51 +0900

    basetime entity zoned->local 복구

--- src/main/java/com/sososhopping/server/entity/BaseTimeEntity.java
+++ src/main/java/com/sososhopping/server/entity/BaseTimeEntity.java
@@ -5,9 +5,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
@@ -21,8 +19,16 @@ public abstract class BaseTimeEntity {
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
-    @NotNull
-    @LastModifiedDate
     private LocalDateTime updatedAt;
 
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
