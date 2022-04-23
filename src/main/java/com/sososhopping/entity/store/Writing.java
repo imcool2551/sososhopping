da988b212e9fb9896655c376d7f09b602475c892
imcool2551 <imcool2551@gmail.com>
2022-04-23 23:59:52 +0900

    ~9.writing

--- src/main/java/com/sososhopping/entity/store/Writing.java
+++ src/main/java/com/sososhopping/entity/store/Writing.java
@@ -6,45 +6,40 @@ import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 
-import static javax.persistence.FetchType.*;
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
 
-@Builder
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 public class Writing extends BaseTimeEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = IDENTITY)
     @Column(name = "writing_id")
     private Long id;
 
-    @NotNull
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "store_id")
     private Store store;
 
-    @NotNull
     private String title;
 
-    @NotNull
     @Column(columnDefinition = "TEXT")
     private String content;
 
-    @NotNull
     @Enumerated(EnumType.STRING)
     private WritingType writingType;
 
-    @Column(length = 512)
     private String imgUrl;
 
-    // 연관 관계 편의 메서드
-    public void setStore(Store store) {
+    @Builder
+    public Writing(Store store, String title, String content, WritingType writingType) {
         this.store = store;
-        this.store.getWritings().add(this);
+        this.title = title;
+        this.content = content;
+        this.writingType = writingType;
     }
 
     // 비즈니스 로직
