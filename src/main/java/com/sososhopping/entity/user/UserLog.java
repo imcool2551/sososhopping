3742abb50deded30528c555de74901d096463206
imcool2551 <imcool2551@gmail.com>
2022-05-10 13:06:42 +0900

    test: UserReport

--- src/main/java/com/sososhopping/entity/user/UserLog.java
+++ src/main/java/com/sososhopping/entity/user/UserLog.java
@@ -14,6 +14,7 @@ import static javax.persistence.FetchType.*;
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
 public class UserLog extends BaseTimeEntity {
 
     @Id
