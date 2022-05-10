d37c4fad606972c97809454b0f7deb904f29fd86
imcool2551 <imcool2551@gmail.com>
2022-05-10 13:19:11 +0900

    test: Coupon

--- src/main/java/com/sososhopping/entity/user/UserLog.java
+++ src/main/java/com/sososhopping/entity/user/UserLog.java
@@ -1,14 +1,13 @@
 package com.sososhopping.entity.user;
 
-import com.sososhopping.entity.common.BaseTimeEntity;
 import com.sososhopping.entity.common.AccountStatus;
+import com.sososhopping.entity.common.BaseTimeEntity;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 
-import static javax.persistence.FetchType.*;
+import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
@@ -22,12 +21,10 @@ public class UserLog extends BaseTimeEntity {
     @Column(name = "user_log_id")
     private Long id;
 
-    @NotNull
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @NotNull
     @Enumerated(EnumType.STRING)
     private AccountStatus active;
 
